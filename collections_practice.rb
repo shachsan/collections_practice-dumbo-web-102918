2a25283905589089907f3ac6251447444ab6d719
SANJAY K SAI <shachsan@hotmail.com>
2018-10-30 02:59:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,28 @@ end
 def sort_array_char_count(arr)
   arr.sort_by{|ele|ele.length}
 end
+
+def swap_elements(arr)
+  arr.map.with_index{|ele, i|i == 1 ? ele[i],ele[i+1]=ele[i+1],ele[i] : ele}
+
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(names)
+  names.map {|name|name.chars.map{|ch|ch[2]=$}.join}
+end
+
+def find_a(words)
+  words.select {|word|word[0]=='a'}
+end
+
+def sum_array(arr)
+  arr.reduce(:+)
+end
+
+def add_s(words)
+  words.map { |word|word+"s"}
+end
